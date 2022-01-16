4c3ddd3c30bddae3e6d144608169ba2adbbb407c
Enrico Saporiti <46729708+EnricoSaporiti@users.noreply.github.com>
2022-01-16 19:06:51 +0100

    Commit2

--- 1_promise_example.js
+++ 1_promise_example.js
@@ -1,3 +1,28 @@
 "use strict"
 
-var fatchSomeData = new Promise( resolve, reject) 
+var notAllGood = true;
+
+//Define a Promise
+var fetchSomeData = new Promise( ( resolve, reject) => {
+    
+    if(!notAllGood) {
+        reject("Error fething data");
+    }
+    
+    resolve( {
+        id: 1,
+        message: 'nice work!';
+
+    }
+
+        )
+} );
+
+
+// Comsume promise
+fetchSomeData.then(fetchedData => {
+    console.log('then' , fetchedData)
+    
+}).catch(err => {
+    console.error('catch:', err);
+})
